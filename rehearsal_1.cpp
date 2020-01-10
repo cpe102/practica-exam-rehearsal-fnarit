50fc3cdb26280548018d620ae1498b1194ca782f
fnarit <naritsararat@gmail.com>
2020-01-10 17:16:14 +0700

    1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+int main (){
+    string name;
+    double grade;
+    cout << "What is your name?: ";
+    cin >> name;
+    cout << "What is your GPA?: ";
+    cin >> grade;
+    if(grade >= 3.5){
+        cout << name << " " << "InW Jrim Jrim!!!";
+    }else{
+        cout << "Try harder, " << name << "!!!";
+    }
+    return 0;
+}
